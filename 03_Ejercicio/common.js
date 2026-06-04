8c316afd7c87e9d0c63ee1255d60b6f645d81c92
@lralvarez <you@example.com>
2026-06-04 12:40:53 +0200

    Pedazo de aplicación

--- 03_Ejercicio/common.js
+++ 03_Ejercicio/common.js
@@ -1,2 +1,10 @@
 
 const urlServicio = "http://localhost:3000/"
+
+function mostrarMensaje(){
+    let mensaje = sessionStorage.getItem("mensaje")
+    if(mensaje){
+        $("#mensaje").html(`<h3>${mensaje}</h3>`)
+        sessionStorage.removeItem("mensaje")
+    }
+}
